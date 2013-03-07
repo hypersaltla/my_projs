089f5c78554fae170071b6d44eede0c037a9cd66
hypersaltla <hypersaltla@hotmail.com>
2013-03-06 22:13:40 -0800

    change merge.py

--- merge.py
+++ merge.py
@@ -1,19 +1,38 @@
 alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-with open("getty_data_img/all_artists.json","w") as fh:
+dirname = "getty_data/"
+
+with open("all_artists.json","w") as fh:
+    fh.write("[\n")
     count = 0
     while count < 26:
-        with open("getty_data_img/artist_" + alphas[count] +".json","r") as fe:
+        with open(dirname+"artist_" + alphas[count] +".json","r") as fe:
+            prev_line = fe.readline().strip()
             for line in fe:
-                if line != "\n":
-                    fh.write(line)
+                if line.strip() != "":
+                    fh.write(prev_line+","+"\n")
+                    prev_line = line.strip()
+            fh.write(prev_line)
+            if count < 25 and prev_line != "":
+                fh.write(",")
+            fh.write("\n")
         count += 1
-with open("getty_data_img/all_collections.json","w") as fc:
+    fh.write("]")
+    
+with open("all_collections.json","w") as fc:
+    fc.write("[\n")
     count = 0
     while count < 26:
-        with open("getty_data_img/artwork_"+alphas[count] +".json","r") as fe:
+        with open(dirname+"artwork_"+alphas[count] +".json","r") as fe:
+            prev_line = fe.readline().strip()
             for line in fe:
-                if line != "\n":
-                    fc.write(line)
+                if line.strip() != "":
+                    fc.write(prev_line+","+"\n")
+                    prev_line = line.strip()
+            fc.write(prev_line)
+            if count < 25 and prev_line != "":
+                fc.write(",")
+            fc.write("\n")
         count += 1
+    fc.write("]")
         
